ab8502e3cf9ec2970f88df3c536559869381730d
Javier Arias <apedro@unicauca.edu.co>
2025-08-25 18:18:37 -0500

    Se agrega transaccionabilidad

--- infrastructure/driven-adapters/r2dbc-postgresql/src/main/java/co/com/pragma/r2dbc/UserReactiveRepositoryAdapter.java
+++ infrastructure/driven-adapters/r2dbc-postgresql/src/main/java/co/com/pragma/r2dbc/UserReactiveRepositoryAdapter.java
@@ -6,18 +6,25 @@ import co.com.pragma.r2dbc.entity.UserEntity;
 import co.com.pragma.r2dbc.helper.ReactiveAdapterOperations;
 import org.reactivecommons.utils.ObjectMapper;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.reactive.TransactionalOperator;
 import reactor.core.publisher.Mono;
 
 @Repository
 public class UserReactiveRepositoryAdapter extends ReactiveAdapterOperations<User, UserEntity,Long, UserReactiveRepository> implements UserRepository {
 
-    public UserReactiveRepositoryAdapter(UserReactiveRepository repository, ObjectMapper mapper) {
+    private final TransactionalOperator transactionalOperator;
+
+    public UserReactiveRepositoryAdapter(UserReactiveRepository repository, ObjectMapper mapper, TransactionalOperator transactionalOperator) {
         super(repository, mapper, d -> mapper.map(d, User.class));
+        this.transactionalOperator = transactionalOperator;
     }
 
     @Override
     public Mono<User> saveUser(User user) {
-        return super.save(user);
+        return transactionalOperator.execute(status ->
+                        super.save(user)
+                ).single()
+                .onErrorResume(Mono::error);
     }
 
     @Override
